41b77720e45713fe6b674f3b06fc90a0b7ab2d95
kenvilsouza <k.c.souza@swansea.ac.uk>
2026-02-03 11:16:03 +0000

    added functions

---
+++ functions.r
@@ -0,0 +1,55 @@
+## The format of sample identifiers/barcodes is described here:
+## https://docs.gdc.cancer.gov/Encyclopedia/pages/TCGA_Barcode/
+##
+## Here is a summary:
+## e.g. TCGA-3C-AAAU-01A-11D-A41Q-05
+##   project TCGA
+##   tissue source site 3C
+##   participant AAAU
+##   sample 01 (01-09 tumor, 10-19 normal, 20-29 controls)
+##   vial A
+##   portion 11
+##   analyte D (as in DNA)
+##   plate A41Q
+##   analysis center 05
+##
+## The following function extracts the participant identifier
+## from a sample id/barcode.
+
+extract.participant <- function(id) {
+  sub("TCGA-[^-]+-([^-]+)-.*", "\\1", id)
+}
+
+extract.tissue <- function(id) {
+  sub("TCGA-[^-]+-[^-]+-([0-9]+)[^-]+-.*", "\\1", id)
+}
+is.tumor.sample <-function(id){
+    tissue.code = extract.tissue(id)
+    as.integer(tissue.code) >= 1 & as.integer(tissue.code) <= 9
+}
+
+## function for extracting tcga tar.gz's to named output
+extract.file <- function(tar.file, extract.file, new.file, resultsdir) {
+  # get file path to extracted file
+  x.file <-
+    grep(extract.file,
+      untar(tar.file, list = T),
+      value = T
+    )
+    
+  # extract the tar file
+  cat("Extracting", tar.file, "to", new.file, "\n")
+  untar(tar.file, exdir=resultsdir, extras="--no-same-owner")
+  x.file = file.path(resultsdir,x.file)
+
+  # move the data to named output
+  file.copy(x.file, new.file)
+
+  # remove untared directory
+  unlink(dirname(x.file), recursive = TRUE)
+}
+
+my.write.table <- function(x, filename) {
+  cat("saving", basename(filename), "...\n")
+  write.table(x, file = filename, row.names = T, col.names = T, sep = "\t")
+}
